8f86f0c28b580377e0e11baf80c4676964903d81
vladian95 <kvankvan1995@gmail.com>
2021-07-16 18:48:46 +0200

    added arrears

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
 $(function(){
 
-
+    $('.arrears__tabs-inner .tab').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.arrears__tabs-inner').find('.tab-item').removeClass('active-tab').hide();
+        $('.arrears__tabs-inner .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
 
     
 });
